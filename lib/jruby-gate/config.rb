ee406b194e1592172466d1e154c2821f7a9f9792
denen99 <adam@dberg.org>
2010-03-08 23:35:30 -0500

    added config.rb

---
+++ lib/jruby-gate/config.rb
@@ -0,0 +1,20 @@
+class JrubyGate
+
+  module Configuration
+
+    ## Load up the necessary jars
+    def initialize
+     ENV['GATE_HOME'] ? '' : raise("You must set GATE_HOME before continuing")
+     Dir[ENV['GATE_HOME'] + "/lib/*.jar"].each { |jar|
+       require  jar
+     }
+     require ENV['GATE_HOME'] + '/bin/gate.jar'
+    end
+  end
+
+  module JavaIO
+   include_package "java.io.File"
+   include_package "java.io"
+  end
+
+end
